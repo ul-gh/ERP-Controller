11c6b786f1cac0d6a928273540a2090558239edf
Ulrich Lukas <opensource@lukas-elektronik.de>
2026-07-25 14:31:07 +0200

    Add more DIDs

--- src/uds_connector/uds_connector.py
+++ src/uds_connector/uds_connector.py
@@ -26,50 +26,67 @@ from uds_connector.python_iso_tp_client import PythonIsoTpClient
 if TYPE_CHECKING:
     from udsoncan.typing import ClientConfig
 
+# Transport protocol IDs for the EVC unit
+# In case of transport over 11-bit CAN, this is identical to the CAN IDs.
+TP_ID_EVC_REQUEST = 0x7E8
+TP_ID_EVC_RESPONSE = 0x7EC
+
 # HV Voltage
-DID_V_HV = 0x3203
-# Battery Current
-DID_I_BATT = 0x3204
+DID_HV_V = 0x3203
+# Charging/Discharging Current
+DID_HV_A = 0x3204
 # SOC
 DID_SOC = 0x2002
+# SOH
+DID_SOH = 0x2006
+# Battery Temperature
+DID_BATT_TEMP = 0x2001
 
 def print_response(response: ReadDataByIdentifier.InterpretedResponse) -> None:
     """Print requested values from response object."""
     # Extract Values
     values = response.service_data.values
-    v_hv = values[DID_V_HV]  # pyright: ignore[reportAny]
-    i_batt = values[DID_I_BATT] # pyright: ignore[reportAny]
-    soc = values[DID_SOC] # pyright: ignore[reportAny]
+    hv_v = values[DID_HV_V]  # pyright: ignore[reportAny]
+    hv_a = values[DID_HV_A]  # pyright: ignore[reportAny]
+    soc = values[DID_SOC]  # pyright: ignore[reportAny]
+    soh = values[DID_SOH]  # pyright: ignore[reportAny]
+    batt_temp = values[DID_BATT_TEMP]  # pyright: ignore[reportAny]
 
     print(
         "Interpreted Response Data:\n",
-        f"V_HV: {v_hv} V\n",
-        f"I_BATT: {i_batt} A\n",
+        f"HV_V: {hv_v} V\n",
+        f"HV_A: {hv_a} A\n",
         f"SOC: {soc} %\n",
+        f"SOH: {soh} %\n",
+        f"BATT_TEMP: {batt_temp} °C\n",
     )
 
 
 def make_uds_request() -> None:
     """Request the specified DIDs from ECU via UDS."""
-    dids_requested = [
-        DID_V_HV,
-        DID_I_BATT,
+    dids_requested: list[int] = [
+        DID_HV_V,
+        DID_HV_A,
         DID_SOC,
+        DID_SOH,
+        DID_BATT_TEMP,
     ]
 
     tp_address = isotp.Address(
         isotp.AddressingMode.Normal_11bits,
         #isotp.AddressingMode.Extended_29bits,
         # txid=0x7DF,7E0,7E4 and rxid=0x7E8,7EC are good candidates
-        txid=0x7E4,
-        rxid=0x7EC,
+        txid=TP_ID_EVC_REQUEST,
+        rxid=TP_ID_EVC_RESPONSE,
     )
 
     client_config: ClientConfig = udsoncan.configs.default_client_config.copy()
     client_config["data_identifiers"] = {
-        DID_V_HV: Fixed16Codec(0.5),
-        DID_I_BATT: Fixed16Codec(0.25, 32768),
+        DID_HV_V: Fixed16Codec(0.5),
+        DID_HV_A: Fixed16Codec(0.25, 32768),
         DID_SOC: Fixed16Codec(0.02),
+        DID_SOH: Fixed16Codec(0.01),
+        DID_BATT_TEMP: Fixed16Codec(1.0, 40),
     }
 
     with PythonIsoTpClient(tp_address, client_config) as client:
